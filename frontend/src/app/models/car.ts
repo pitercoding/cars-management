fcd2f183dcd030c25ba81205a83f499e44c2552f
pitercoding <piterg.bio@gmail.com>
2025-12-20 10:53:04 +0100

    feat(cars): show owner full name and driver license in cars list using @if syntax

--- frontend/src/app/models/car.ts
+++ frontend/src/app/models/car.ts
@@ -1,5 +1,6 @@
 import { Accessory } from './accessory';
 import { Brand } from './brand';
+import { Owner } from './owner';
 
 export class Car {
   id?: number;
@@ -7,7 +8,8 @@ export class Car {
   model: string = '';
   manufactureYear: number = new Date().getFullYear();
   brand?: Brand;
-  accessories!: Accessory[];
+  accessories: Accessory[] = [];
+  owner?: Owner;
 
   constructor(init?: Partial<Car>) {
     Object.assign(this, init);
